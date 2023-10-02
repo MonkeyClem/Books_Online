8290c93881c163b2744070c225b5b244cb4c0cab
Clement Jeulin <clement93jeulin@gmail.com>
2023-10-02 02:26:06 +0200

    First commit : imports

---
+++ script.py
@@ -0,0 +1,11 @@
+# Nous importons le module csv, présent dans la bibliothèque standard de Python et 
+# qui fournit des fonctionnalités pour lire et écrire des fichiers CSV.
+# Il sera utilisé pour écrire les données extraites dans un fichier CSV.
+import csv
+
+# Nous importons requests, qui est une bibliothèque cliente HTTP, spécialement conçue pour le langage de programmation Python. 
+import requests
+
+# Nous importons la classe BeautifulSoup du module bs4. BeautifulSoup est une bibliothèque qui est utiisée pour 
+# le parsing (analyse) de documents HTML ou XML. Ici, elle est utilisée pour extraire des informations spécifiques de la page web.
+from bs4 import BeautifulSoup
